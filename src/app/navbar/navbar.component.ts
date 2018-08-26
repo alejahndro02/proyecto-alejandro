aedc51b39195278f7a0d7270bc2803e8bb2c2b35
alejahndro <alejahndro02@gmail.com>
2018-08-25 22:56:20 -0500

    Configuracion forma responsive inicio y navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-navbar',
@@ -10,6 +11,26 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+//Funcion de desĺegue de slide izquierdo al hacer click enen boton menu
+    $(document).ready(main);
+    var contador = 1;
+    function main(){
+	  $('.menu_bar').click(function(){
+		  if(contador == 1){
+		  	$('nav').animate({
+		  		left: '0%'
+		  	});
+		  	contador = 0;
+		  } else {
+		  	  contador = 1;
+		  	  $('nav').animate({
+		  	  	left: '-100%'
+		  	  });
+		    }
+ 
+	  });
+ 
+    };
   }
 
 }
